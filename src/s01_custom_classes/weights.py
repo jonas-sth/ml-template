3a81b409462fe53e30c3c1227919551853165556
jonas_sth <jonas_st@icloud.com>
2022-10-12 15:43:07 +0200

    updated weight init function

--- src/s01_custom_classes/weights.py
+++ src/s01_custom_classes/weights.py
@@ -3,5 +3,5 @@ from torch import nn
 
 def xavier_weight_init(m):
     if isinstance(m, nn.Conv2d):
-        nn.init.xavier_uniform(m.weight)
+        nn.init.xavier_uniform_(m.weight)
         m.bias.data.fill_(0.01)
