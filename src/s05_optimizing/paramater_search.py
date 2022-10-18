53654dd724a5762c6a67cfe66cc230eacc2cb2b5
jonas_sth <jonas_st@icloud.com>
2022-10-18 13:36:12 +0200

    Added base for augmentation comparison

--- src/s05_optimizing/paramater_search.py
+++ src/s05_optimizing/paramater_search.py
@@ -5,6 +5,7 @@ import shutil
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
+from torchvision.transforms import transforms
 
 from src.s00_utils import constants
 from src.s02_customizing import datasets, models, weight_inits, trainers
@@ -69,7 +70,70 @@ def tune_sgd_optimizer(file_path, dir_path, clear_dir=True):
     comparison_writer.close()
 
 
+def tune_augmentation(file_path, dir_path, clear_dir=True):
+    # Clean up
+    if clear_dir:
+        shutil.rmtree(dir_path, ignore_errors=True)
+
+    # Set parameters to test
+    possible_transformations = {
+        "Standard": transforms.Compose([transforms.ToTensor(),
+                                        transforms.Grayscale(),
+                                        transforms.Normalize(0.5, 0.2)]
+                                       ),
+        "Flipping": transforms.Compose([transforms.ToTensor(),
+                                        transforms.Grayscale(),
+                                        transforms.Normalize(0.5, 0.2),
+                                        transforms.RandomVerticalFlip()]
+                                       ),
+        "Inverting": transforms.Compose([transforms.ToTensor(),
+                                         transforms.Grayscale(),
+                                         transforms.Normalize(0.5, 0.2),
+                                         transforms.RandomInvert()]
+                                        )
+    }
+
+    total_possibilities = len(possible_transformations)
+
+    # Initialize logging
+    comparison_writer = SummaryWriter(dir_path)
+
+    # Iterate over possible hyper parameters
+    i = 1
+    for name, transformation in possible_transformations.items():
+        # Load trainer
+        trainer = trainers.CustomKFoldTrainer.from_file(file_path)
+
+        # Replace transformation in Dataset
+        trainer.data.transform = transformation
+
+        # Set output directory
+        exp_dir = os.path.join(dir_path, f"run_{i}")
+
+        # Run training
+        avg, std = trainer.k_fold_cross_validation(exp_dir, run_info=f"{i}/{total_possibilities}")
+
+        # Log to tensorboard
+        comparison_writer.add_hparams(
+            hparam_dict={
+                "transformation": name
+            },
+            metric_dict={
+                "accuracy": avg
+            },
+            hparam_domain_discrete={
+                "transformation": list(possible_transformations.keys())
+            },
+            run_name=f"run_{i}"
+        )
+
+        # Increase counter
+        i += 1
+
+    comparison_writer.close()
+
+
 if __name__ == "__main__":
     config_path = os.path.join(constants.ROOT, r"models\exp\config.pt")
-    output_dir = os.path.join(constants.ROOT, r"models\hyper_search")
-    tune_sgd_optimizer(config_path, output_dir)
+    output_dir = os.path.join(constants.ROOT, r"models\augmentation")
+    tune_augmentation(config_path, output_dir)
